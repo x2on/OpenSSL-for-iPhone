a9b311d58ca7e276c16a6abc516df59d62e51a05
Felix Schulze <code@felixschulze.de>
2010-02-04 15:04:16 +0100

    XCode Projectfor iPhone SDK 3 with OpenSSL-0.9.8m-beta1

---
+++ Classes/OpenSSL_1AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  OpenSSL_1AppDelegate.h
+//  OpenSSL-1
+//
+//  Created by Felix Schulze on 01.02.2010.
+//  Copyright Felix Schulze 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenSSL_1AppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OpenSSL-1
+//
+//  Created by Felix Schulze on 01.02.2010.
+//  Copyright Felix Schulze 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
