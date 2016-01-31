6f7e96be305f246843933021402b6e9236cc5935
Felix Schulze <x2on@users.noreply.github.com>
2016-01-31 19:21:05 +0100

    OpenSSL 1.0.2f
    Fixes #88
    Fixes #89

--- build-libssl.sh
+++ build-libssl.sh
@@ -21,7 +21,7 @@
 ###########################################################################
 #  Change values here                                                     #
 #                                                                         #
-VERSION="1.0.2e"                                                          #
+VERSION="1.0.2f"                                                          #
 IOS_SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`                   #
 TVOS_SDKVERSION=`xcrun -sdk appletvos --show-sdk-version`                 #
 CONFIG_OPTIONS=""                                                         #
