34490f8f39129a7cbb60d914da9c38d5d34f6690
Anton Tieleman <git@oneton.nl>
2016-10-22 13:25:02 +0200

    New Travis build script for parallel 1.0 / 1.1 build

---
+++ travis-build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Log script output with prefix
+log_output()
+{
+  while read INPUT; do
+    echo "${1} ${INPUT}"
+  done
+}
+
+# Shut up curl
+export CURL_OPTIONS="-s"
+
+# Build 1.0.2 in current directory
+./build-libssl.sh --noparallel --verbose-on-error | log_output "ARCHS  " &
+PID_ARCHS=$!
+
+# Build 1.1.0 in (temporary) subdirectory
+mkdir targets
+cd targets
+cp -r ../include .
+../build-libssl.sh --noparallel --verbose-on-error --ec-nistp-64-gcc-128 --version=1.1.0b | log_output "TARGETS" &
+PID_TARGETS=$!
+
+echo "SCRIPT  Started jobs, waiting for jobs to finish"
+wait ${PID_ARCHS}
+wait ${PID_TARGETS}
+
+# Verify/prepare 1.1.0 build dir
+xcrun -sdk iphoneos lipo -info ./lib/*.a | log_output "TARGETS"
+../create-openssl-framework.sh | log_output "TARGETS"
+xcrun -sdk iphoneos lipo -info openssl.framework/openssl | log_output "TARGETS"
+cp -r ../OpenSSL-for-* .
+
+# Back to main dir
+cd ..
+xcrun -sdk iphoneos lipo -info ./lib/*.a | log_output "ARCHS  "
+./create-openssl-framework.sh | log_output "ARCHS  "
+xcrun -sdk iphoneos lipo -info openssl.framework/openssl | log_output "ARCHS  "
+
