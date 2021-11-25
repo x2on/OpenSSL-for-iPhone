5df4fba83ec75d05200f730d39a470173a4f19ca
Davide De Rosa <keeshux@gmail.com>
2021-11-25 09:05:24 +0100

    Make all-in-one script to produce SwiftPM binary
    Disable Bitcode.

--- create-swiftpm-binaries.sh
+++ create-swiftpm-binaries.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-if [ $# == 0 ]; then
-    echo "Usage: `basename $0` static|dynamic"
-    exit 1
-fi
 VERSION="1.1.1l"
 TARGETS="ios-sim-cross-x86_64 ios-sim-cross-arm64 ios64-cross-arm64 ios64-cross-arm64e macos64-x86_64 macos64-arm64 mac-catalyst-x86_64 mac-catalyst-arm64"
 FRAMEWORK="openssl.xcframework"
 FRAMEWORK_ZIP="openssl.xcframework.zip"
 FRAMEWORK_DIR="swiftpm/$VERSION"
-./create-openssl-framework.sh $1
+./build-libssl.sh --version="$VERSION" --targets="$TARGETS" --disable-bitcode
+#./build-libssl.sh --version="$VERSION" --disable-bitcode
+./create-openssl-framework.sh dynamic
 ( cd frameworks && zip -yr $FRAMEWORK_ZIP $FRAMEWORK )
-mkdir -p $FRAMEWORK_DIR/$1
-mv frameworks/$FRAMEWORK_ZIP $FRAMEWORK_DIR/$1
-swift package compute-checksum $FRAMEWORK_DIR/$1/$FRAMEWORK_ZIP
+mkdir -p $FRAMEWORK_DIR
+mv frameworks/$FRAMEWORK_ZIP $FRAMEWORK_DIR
+swift package compute-checksum $FRAMEWORK_DIR/$FRAMEWORK_ZIP
