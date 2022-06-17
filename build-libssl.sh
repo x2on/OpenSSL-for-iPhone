797a82d20a3d591bd4893f78a7abf05e7711adc7
Davide De Rosa <keeshux@gmail.com>
2022-06-17 09:24:06 +0200

    Bump to 1.1.1o

--- build-libssl.sh
+++ build-libssl.sh
@@ -25,7 +25,7 @@ set -u
 # SCRIPT DEFAULTS
 
 # Default version in case no version is specified
-DEFAULTVERSION="1.1.1m"
+DEFAULTVERSION="1.1.1o"
 
 # Default (=full) set of targets (OpenSSL >= 1.1.1) to build
 DEFAULTTARGETS=`cat <<TARGETS
