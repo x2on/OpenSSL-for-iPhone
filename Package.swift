1c03f4d3fc1d90a0a525af68f06eada56e5e8bb1
Davide De Rosa <keeshux@gmail.com>
2022-06-17 07:38:55 +0000

    OpenSSL 1.1.1o

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/keeshux/openssl-apple/releases/download/1.1.11301/openssl.xcframework.zip",
-            checksum: "253b374dcb2b24cfd6e3c4c1bc38b1bc21aee9c75c2053bb06cfee04fc9d9965")
+            url: "https://github.com/keeshux/openssl-apple/releases/download/1.1.11500/openssl.xcframework.zip",
+            checksum: "6e053c4bc25ce38510bb917bb3f21eed4558311e6c5f6dfd0ee68cfc8bbae130")
     ]
 )
